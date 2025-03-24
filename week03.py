6a6668de163b1a97cb08eeff9f0d738a9a0c83b4
batjjong <batjjong@gmail.com>
2025-03-24 11:59:34 +0900

    0.9 array

--- week03.py
+++ week03.py
@@ -1,10 +1,25 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-#  ratings = [1, 2, 4, 3, 100]
-ratings = [1, 2, 4, 3]
-
-group_rating = list(zip(groups, ratings))
-print(group_rating)
 city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
 # city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
 city = set(city)
 print(city)
+
+
+def duplicate_city(cities):
+    result = list()
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result.append(city)
+    return result
+
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+# cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities)))
