b302afc9b6f4aff5b681627997b77455a0f2629a
cha80537 <cha80537@gmail.com>
2025-04-07 10:09:41 +0900

    v1.5 linked list

--- wee4.py
+++ wee4.py
@@ -1,42 +1,55 @@
 import random
 
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
-        self.link = link
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+@ @-9
 
-    def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.link:
+, 14 + 11, 25 @ @
+
+
+def __init__(self):
+    self.head = None
+
+
+def append(self, data):
+    if not self.head:  # 링크드리스트가 노드가 하나도 없는 상태면
+        self.head = Node(data)  # 새 노드를 head에 연결
+    if not self.head:
+        self.head = Node(data)
+        return
+    current = self.head
+    while current.link:
+        current = current.link  # 다음노드로 이동
+        current = current.link
+    current.link = Node(data)
+
+
+def search(self, target):
+    current = self.head
+    while current.link:
+        if current.data == target:
+            return f"{target}을(를) 찾았습니다"
+        else:
             current = current.link
-        current.link = Node(data)
+    return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
 
 
-    def search(self, target):
-        current = self.head
-        while current.link:
-            if current.data == target:
-                return f"{target}을(를) 찾았습니다"
-            else:
-                current = current.link
-        return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
+def __str__(self):
+    node = self.head
+    out_texts = ""
 
 
-    def __str__(self):
-        node = self.head
-        out_texts = ""
-        while node is not None:
-            out_texts = out_texts + f"{node.data} -> "
-            node = node.link
-        return out_texts + "end"
+@ @-26
+
+, 8 + 39, 17 @ @
+
+
+def __str__(self):
+    return out_texts + "end"
 
 
 # ll = LinkedList()
@@ -48,6 +61,9 @@ class LinkedList:
 # print(ll.search(10))
 
 ll = LinkedList()
+ll.append(8)
+ll.append(10)
+ll.append(-9)
 for _ in range(10):
     ll.append(random.randint(1, 30))
 
