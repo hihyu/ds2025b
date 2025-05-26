2cde2d88f67deed16fd1770f574035d3ed14c73c
cha80537 <cha80537@gmail.com>
2025-05-26 10:18:56 +0900

    4.5 deque

--- week11.py
+++ week11.py
@@ -1,29 +1,8 @@
-graph = [
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0]
-]
+from collections import deque
 
-def dfs(g, i, visited):
-    visited[i] = 1
-    print(chr(ord('A')+i), end=' ')
-    for j in range(len(g)):
-        if g[i][j] == 1 and not visited[j]:
-            dfs(g, j, visited)
-
-
-def bfs(g, i, visited):
-    pass
-
-
-visited_dfs = [0 for _ in range(len(graph))]
-dfs(graph, 7, visited_dfs)
-visited_bfs = [0 for _ in range(len(graph))]
-dfs(graph, 7, visited_dfs)
-print()
-bfs(graph, 6, visited_bfs)
+d = deque([3, -9, 14])
+d.append(77)
+# d.appendleft(100)
+d.append(91)
+for i in range(len(d)):
+    print(d.popleft())
